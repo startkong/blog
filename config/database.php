f7ec425f8bdc9c68755ee4d884f038ffce057faa
xuheng <1197256140@qq.com>
2020-08-18 16:12:50 +0800

    gegnxin

--- config/database.php
+++ config/database.php
@@ -32,9 +32,9 @@ return [
             // 数据库名
             'database'          => 'camillezhou_net',
             // 用户名
-            'username'          => 'root',
+            'username'          => 'camillezhou_net',
             // 密码
-            'password'          => '123456',
+            'password'          => 'JeCFK6mCrKKnGKeP',
             // 端口
             'hostport'          => '3306',
             // 数据库连接参数
